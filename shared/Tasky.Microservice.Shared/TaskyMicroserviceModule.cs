131d287e9f8a26675c7b138cea0dd363cd1d2ec4
Anto Subash <antosubash@live.com>
2022-03-06 20:33:25 +0100

    wip

---
+++ shared/Tasky.Microservice.Shared/TaskyMicroserviceModule.cs
@@ -0,0 +1,21 @@
+namespace Tasky.Microservice.Shared;
+
+[DependsOn(
+    typeof(AbpAutofacModule)
+)]
+public class TaskyMicroserviceModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpDbConnectionOptions>(options =>
+        {
+            options.Databases.Configure("Administration", database =>
+            {
+                database.MappedConnections.Add("AbpAuditLogging");
+                database.MappedConnections.Add("AbpPermissionManagement");
+                database.MappedConnections.Add("AbpSettingManagement");
+                database.MappedConnections.Add("AbpFeatureManagement");
+            });
+        });
+    }
+}
