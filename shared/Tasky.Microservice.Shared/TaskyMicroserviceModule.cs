04a21c6e72005d28689fd8714c592b8ee353771c
Anto Subash <antosubash@live.com>
2022-03-07 18:47:50 +0100

    AbpDbConnectionOptions

--- shared/Tasky.Microservice.Shared/TaskyMicroserviceModule.cs
+++ shared/Tasky.Microservice.Shared/TaskyMicroserviceModule.cs
@@ -15,13 +15,30 @@ public class TaskyMicroserviceModule
 
         Configure<AbpDbConnectionOptions>(options =>
         {
-            options.Databases.Configure("Administration", database =>
-            {
-                database.MappedConnections.Add("AbpAuditLogging");
-                database.MappedConnections.Add("AbpPermissionManagement");
-                database.MappedConnections.Add("AbpSettingManagement");
-                database.MappedConnections.Add("AbpFeatureManagement");
-            });
+                options.Databases.Configure("SaasService", database =>
+                {
+                    database.MappedConnections.Add("AbpTenantManagement");
+                    database.IsUsedByTenants = false;
+                });
+
+                options.Databases.Configure("AdministrationService", database =>
+                {
+                    database.MappedConnections.Add("AbpAuditLogging");
+                    database.MappedConnections.Add("AbpPermissionManagement");
+                    database.MappedConnections.Add("AbpSettingManagement");
+                    database.MappedConnections.Add("AbpFeatureManagement");
+                });
+                
+                options.Databases.Configure("IdentityService", database =>
+                {
+                    database.MappedConnections.Add("AbpIdentity");
+                    database.MappedConnections.Add("AbpIdentityServer");
+                });
+                
+                options.Databases.Configure("ProjectService", database =>
+                {
+                    database.MappedConnections.Add("ProjectService");
+                });
         });
     }
 }
