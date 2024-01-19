529f0326b6bab775e576dad489c7ecd335bbb4da
Anto Subash <antosubash@live.com>
2024-01-19 17:34:51 +0100

    updated the angular project

--- src/apps/angular/src/app/app.module.ts
+++ src/apps/angular/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { registerLocale } from '@abp/ng.core/locale';
 import { IdentityConfigModule } from '@abp/ng.identity/config';
 import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
 import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
-import { ThemeLeptonXModule } from '@abp/ng.theme.lepton-x';
-import { SideMenuLayoutModule } from '@abp/ng.theme.lepton-x/layouts';
 import { ThemeSharedModule } from '@abp/ng.theme.shared';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -14,7 +12,11 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_ROUTE_PROVIDER } from './route.provider';
-import { AbpOAuthModule } from "@abp/ng.oauth";
+import { FeatureManagementModule } from '@abp/ng.feature-management';
+import { AbpOAuthModule } from '@abp/ng.oauth';
+import { ThemeLeptonXModule } from '@abp/ng.theme.lepton-x';
+import { SideMenuLayoutModule } from '@abp/ng.theme.lepton-x/layouts';
+import { AccountLayoutModule } from '@abp/ng.theme.lepton-x/account';
 @NgModule({
   imports: [
     BrowserModule,
@@ -26,12 +28,17 @@ import { AbpOAuthModule } from "@abp/ng.oauth";
     }),
     AbpOAuthModule.forRoot(),
     ThemeSharedModule.forRoot(),
+    
     AccountConfigModule.forRoot(),
     IdentityConfigModule.forRoot(),
     TenantManagementConfigModule.forRoot(),
     SettingManagementConfigModule.forRoot(),
-    ThemeLeptonXModule.forRoot(),
-    SideMenuLayoutModule.forRoot(),
+    
+    
+    FeatureManagementModule.forRoot(),
+              ThemeLeptonXModule.forRoot(),
+              SideMenuLayoutModule.forRoot(),
+              AccountLayoutModule.forRoot(),
   ],
   declarations: [AppComponent],
   providers: [APP_ROUTE_PROVIDER],
