7d71a30d973dbbe6be1fff7ca9f2f6be650b0708
Anto Subash <antosubash@live.com>
2024-02-11 13:40:17 +0100

    fix: added iis settings

--- src/Tasky.AppHost/Program.cs
+++ src/Tasky.AppHost/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Tasky_AuthServer>("tasky.authserver");
+
 builder.AddProject<Projects.Tasky_Administration_HttpApi_Host>("tasky.administration.httpapi.host");
 
 builder.AddProject<Projects.Tasky_IdentityService_HttpApi_Host>("tasky.identityservice.httpapi.host");
@@ -10,10 +12,9 @@ builder.AddProject<Projects.Tasky_SaaS_HttpApi_Host>("tasky.saas.httpapi.host");
 
 builder.AddProject<Projects.Tasky_Gateway>("tasky.gateway");
 
-//builder.AddProject<Projects.Tasky_Blazor_Server>("tasky.blazor.server");
+builder.AddProject<Projects.Tasky_Blazor_Server>("tasky.blazor.server");
 
 builder.AddProject<Projects.Tasky_Blazor>("tasky.blazor");
 
-builder.AddProject<Projects.Tasky_AuthServer>("tasky.authserver");
 
 builder.Build().Run();
