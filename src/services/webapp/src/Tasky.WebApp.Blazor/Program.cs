34e448a683d4c35d7131e8117ad52d23107f047d
Doug Wilson <d@dkw.io>
2024-11-27 20:23:21 -0800

    Cleanup

--- src/services/webapp/src/Tasky.WebApp.Blazor/Program.cs
+++ src/services/webapp/src/Tasky.WebApp.Blazor/Program.cs
@@ -27,21 +27,15 @@ internal class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseWebAssemblyDebugging();
-        }
-        else
-        {
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
         }
 
         app.UseHttpsRedirection();
 
         app.MapStaticAssets();
-        app.UseAntiforgery();
+        //app.UseAntiforgery();
 
         app.MapRazorComponents<App>()
            .AddInteractiveWebAssemblyRenderMode()
