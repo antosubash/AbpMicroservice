17d6bfe2bc6523b2ad1d6bf9e3056bf06a4dbcf6
Washington Acero M <shin321654987@gmail.com>
2025-02-07 14:21:35 -0800

    fixed file build props file name, angular proyect for run with abp 9 and replaced leptonx by basic theme

--- src/apps/angular/src/app/app.module.ts
+++ src/apps/angular/src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { AccountConfigModule } from '@abp/ng.account/config';
-import { CoreModule } from '@abp/ng.core';
+import { CoreModule, provideAbpCore, withOptions } from '@abp/ng.core';
+import { provideAbpOAuth } from '@abp/ng.oauth';
+import { provideSettingManagementConfig } from '@abp/ng.setting-management/config';
+import { provideFeatureManagementConfig } from '@abp/ng.feature-management';
+import { ThemeSharedModule, provideAbpThemeShared } from '@abp/ng.theme.shared';
+import { provideIdentityConfig } from '@abp/ng.identity/config';
+import { provideAccountConfig } from '@abp/ng.account/config';
+import { provideTenantManagementConfig } from '@abp/ng.tenant-management/config';
 import { registerLocale } from '@abp/ng.core/locale';
-import { IdentityConfigModule } from '@abp/ng.identity/config';
-import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
-import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
-import { ThemeSharedModule } from '@abp/ng.theme.shared';
+import { ThemeBasicModule, provideThemeBasicConfig } from '@abp/ng.theme.basic';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,36 +15,36 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_ROUTE_PROVIDER } from './route.provider';
-import { FeatureManagementModule } from '@abp/ng.feature-management';
-import { AbpOAuthModule } from '@abp/ng.oauth';
-import { ThemeLeptonXModule } from '@abp/ng.theme.lepton-x';
-import { SideMenuLayoutModule } from '@abp/ng.theme.lepton-x/layouts';
-import { AccountLayoutModule } from '@abp/ng.theme.lepton-x/account';
+
 @NgModule({
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    CoreModule.forRoot({
-      environment,
-      registerLocaleFn: registerLocale(),
-    }),
-    AbpOAuthModule.forRoot(),
-    ThemeSharedModule.forRoot(),
-    
-    AccountConfigModule.forRoot(),
-    IdentityConfigModule.forRoot(),
-    TenantManagementConfigModule.forRoot(),
-    SettingManagementConfigModule.forRoot(),
-    
-    
-    FeatureManagementModule.forRoot(),
-              ThemeLeptonXModule.forRoot(),
-              SideMenuLayoutModule.forRoot(),
-              AccountLayoutModule.forRoot(),
+    ThemeSharedModule,
+
+    CoreModule,
+    ThemeBasicModule,
   ],
-  declarations: [AppComponent],
-  providers: [APP_ROUTE_PROVIDER],
+  providers: [
+    APP_ROUTE_PROVIDER,
+
+    provideAbpCore(
+      withOptions({
+        environment,
+        registerLocaleFn: registerLocale(),
+      })
+    ),
+    provideAbpOAuth(),
+    provideIdentityConfig(),
+    provideSettingManagementConfig(),
+    provideFeatureManagementConfig(),
+    provideAccountConfig(),
+    provideTenantManagementConfig(),
+    provideAbpThemeShared(),
+    provideThemeBasicConfig(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
